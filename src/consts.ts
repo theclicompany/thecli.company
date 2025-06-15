18805c910fbcacc70f040a20188c510b3767edc9
lcfd <9001053+fedriz@users.noreply.github.com>
2025-06-15 20:04:58 +0200

    To title case

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "The CLI COMPANY";
+export const SITE_TITLE = "The CLI Company";
 export const SITE_DESCRIPTION = "Less clicking, more doing.";
